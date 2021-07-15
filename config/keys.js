61e0b4fcad260e1ddfce28ae7887a7b429a40ed4
Mohit Jha <55483009+Mohit-17@users.noreply.github.com>
2021-07-15 12:34:44 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,13 +2,13 @@
  
 module.exports = {
     google: {
-        clientID:'751600208207-cum1ndkc11djst2d2deve8i53o46fdar.apps.googleusercontent.com',
-        clientSecret:'KSWPocoSPiaGfObj5I7b1TF6'
+        clientID:'',
+        clientSecret:''
     },
     mongodb: {
-        dbURI:'mongodb+srv://mohit:softeng24@cluster0.a9lln.mongodb.net/Music-Recommendation-System'
+        dbURI:''
     },
     session:{
-        cookieKey:'mywebsiteisawesomeiguess'
+        cookieKey:''
     }
 };  
