820d20b0359c04201f3a2b47fb2af84a48b11f8d
Mohit Jha <55483009+Mohit-17@users.noreply.github.com>
2021-07-15 12:33:44 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//add this file to .gitignore
+ 
+module.exports = {
+    google: {
+        clientID:'751600208207-cum1ndkc11djst2d2deve8i53o46fdar.apps.googleusercontent.com',
+        clientSecret:'KSWPocoSPiaGfObj5I7b1TF6'
+    },
+    mongodb: {
+        dbURI:'mongodb+srv://mohit:softeng24@cluster0.a9lln.mongodb.net/Music-Recommendation-System'
+    },
+    session:{
+        cookieKey:'mywebsiteisawesomeiguess'
+    }
+};  
